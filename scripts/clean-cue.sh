13ef715a8b9b76120cffbda1640ea2536fe88959
Jason Racey <jasonracey@gmail.com>
2020-01-28 05:55:25 -0800

    Remove leading question marks from each line

--- scripts/clean-cue.sh
+++ scripts/clean-cue.sh
@@ -2,7 +2,11 @@
 
 clean () {
   echo "Cleaning $1"
-  perl -i.bak -pe 's/[^[:ascii:]]/?/g' "$1"
+
+  # copy original file to .cue.bak
+  # regex 1: replace all non-ascii characters with a question mark 
+  # regex 2: remove all question marks from the start of each line 
+  perl -i.bak -pe 's/[^[:ascii:]]/?/g;s/^\?{1,}//;' "$1"
 }
 
 find . -iname '*.cue' |
